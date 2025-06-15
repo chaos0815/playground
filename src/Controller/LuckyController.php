6a02b86f20c2565b4cdc9e670a0de308f0f9b0a8
Christian Wolf <christian.wolf@gmail.com>
2025-06-15 17:33:11 +0200

    demo controller

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LuckyController
+{
+    #[Route('/lucky/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
